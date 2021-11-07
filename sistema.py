f0df908df22767713150ba44b9965cab6006481c
Trinidad Boragini <mtboragini@gmail.com>
2021-11-07 18:51:17 -0300

    Creado metodo alta_cliente_individuo en class Usuario_administrador

--- sistema.py
+++ sistema.py
@@ -5,13 +5,15 @@ from class_cliente_individuo import Cliente_individuo
 from class_cliente_pyme import Cliente_pyme
 from class_usuario_administrador import Usuario_administrador
 
-clientes_individuos = {32521: Cliente_individuo(32521, 32521456, "Trinidad", "Boragini", 2035214528, "San Martin 100", 24941546289, 3),
-                       32123: Cliente_individuo(32123, 25487412, "Lucio", "Gronda", 25632145, "Saavedra 42", 4214587, 12)
-                       }
+clientes_individuos = {32521: Cliente_individuo(32521456, "Trinidad", "Boragini", 2035214528, "San Martin 100", 24941546289, 3, 32521, []),
+                       32123: Cliente_individuo(32123, 25487412, "Lucio", "Gronda", 25632145, "Saavedra 42", 4214587, 12, [])}
 clientes_pyme = {1: Cliente_pyme(
     1, "La Pirca", 125487458, "Belgrano 230", 2494561231, [3, 25], [78, 79])}
 
-usuarios = {1: Usuario(1, 12, 3), 2: Usuario(2, 11, 5)}
+usuarios = {
+    1: Usuario(1, 12, 'IRO345', True, False),
+    2: Usuario(2, 11, 'POB123', False, True)
+}
 
 
 class Banco():
