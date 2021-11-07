8fc2bb273c35f6cff7d1ed99aecbe0a7e8eefd86
@pilarmoralescosta <maria.morales@globant.com>
2021-11-07 20:04:14 -0300

    metodos de clase autoridad firmante y cuenta corriente

--- class_aut_firmante.py
+++ class_aut_firmante.py
@@ -1,4 +1,8 @@
+from class_caja_ahorro import Caja_ahorro
+from class_cta_cte import Cuenta_corriente
 from class_persona import Persona
+import random
+import datetime
 
 
 class Autoridad_firmante(Persona):
@@ -6,11 +10,39 @@ class Autoridad_firmante(Persona):
         super.__init__(self, dni, cuit_cuil, nombre,
                        apellido, direccion, telefono, email)
 
-    def cierre_cuenta(self):
-        pass
+    def cierre_cuenta(self, cuenta_a_borrar, cliente):
+        
+        cerrar_cuenta = input("Esta seguro que quiere cerrrar la cuenta?")
+        if cerrar_cuenta == "si":
+            clientes_pyme[cliente.id_cliente].cuentas.remove(cuenta_a_borrar)
+        
 
-    def apertura_cuenta(self):
-        pass
+    def apertura_cuenta(self, cliente_pyme):
+
+        cuenta_a_abrir = int(input("Escriba 1 si quiere abrir una Caja de Ahorro, 2 si quiere abrir una Cuenta Corriente y 3 para salir"))
+        sucursal = input("Elija la sucursal")
+        nro_cuenta = random.int(12)
+        cbu = random.int(15)
+        fecha_apertura = datetime.date()
+        saldo = 0
+        saldo_retenido = False
+        es_bonificada = True
+        
+        if cuenta_a_abrir == 1:
+            tipo = "Caja de Ahorro"
+            clientes_pyme[cliente_pyme.id_cliente].cuentas.append(Caja_ahorro(sucursal, nro_cuenta, cbu, fecha_apertura, saldo, tipo, saldo_retenido, es_bonificada))
+        elif cuenta_a_abrir == 2:
+            tipo = "Cuenta Corriente"
+            clientes_pyme[cliente_pyme.id_cliente].cuentas.append(Cuenta_corriente(sucursal, nro_cuenta, cbu, fecha_apertura, saldo, tipo, saldo_retenido, es_bonificada))
+
+        else:
+            exit()
+        
+
 
     def __str__(self):
         return super().__str__()
+
+
+
+jaimito.apertura_cuenta(amanecer)
--- todos_clientes.py
+++ todos_clientes.py
@@ -1,6 +1,10 @@
+from class_aut_firmante import Autoridad_firmante
 from class_cliente_individuo import Cliente_individuo
 from class_cliente_pyme import Cliente_pyme
 clientes_individuos = {32521 : Cliente_individuo(32521, 32521456, "Trinidad", "Boragini", 2035214528, "San Martin 100", 24941546289, 3),
 32123 : Cliente_individuo(32123, 25487412, "Lucio", "Gronda", 25632145, "Saavedra 42", 4214587, 12)
 }
-clientes_pyme = {1 : Cliente_pyme(1, "La Pirca", 125487458, "Belgrano 230", 2494561231, [3,25], [78,79])}
+clientes_pyme = {
+    1 : Cliente_pyme(1, "La Pirca", 125487458, "Belgrano 230", 2494561231, [3,25], [78,79]),
+2 : Cliente_pyme(2, "El Amanecer", 203564859, "San Martin 650", 2494152032, jchun@chiams.cm, ["autoridad firmante 1"], [123, 453])
+}
