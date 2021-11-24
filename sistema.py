e764680fd4e5664549de41da68235aac720ae9e3
Trinidad Boragini <mtboragini@gmail.com>
2021-11-24 20:29:12 -0300

    Added some coments and refactoring methods in class Sistema

--- sistema.py
+++ sistema.py
@@ -27,37 +27,49 @@ class Banco():
         self.administrador = Usuario_administrador()
         self.usuario_logueado = None
 
-    def alta_usuario(self, dni, id_cliente, usuarios):
+    def alta_usuario(self, dni, id_cliente):
+        '''Método para dar de alta un usuario del sistema. Recive como parametro el dni del usuario y
+        el id del cliente, actualiza el diccionario de usuarios y retorna un mensaje de confirmación'''
+
         es_cliente_ind = False
         es_cliente_pyme = False
 
-        if int(dni) in usuarios:
+        # verificamos que el cliente exista
+        if int(dni) in self.usuarios:
             return print("El usuario ya existe")
+        # si no existe, verificamos si es cliente individuo o cliente pyme
         else:
             clave = input("Ingrese una clave: ")
             tipo_cliente = input(
-                "¿Es un cliente individuo, PyME o ambos? (i/p/a): ").lower()
+                "¿Es un cliente individuo o PyME? (i/p): ").lower()
             if tipo_cliente == "i":
                 es_cliente_ind = True
             elif tipo_cliente == "p":
                 es_cliente_pyme = True
-            elif tipo_cliente == "a":
-                es_cliente_ind = True
-                es_cliente_pyme = True
             else:
                 return print("Ingrese una opción válida")
+
+            # creamos la instancia de Usuario
             nuevo_usuario = Usuario(
                 dni, clave, id_cliente, es_cliente_ind, es_cliente_pyme)
-            usuarios[dni] = nuevo_usuario
+
+            # actualizamos el diccionario de usuarios
+            self.usuarios[dni] = nuevo_usuario
+
             return print(f'\nEl usuario ha sido generado exitosamente: ' + Usuario.__str__(nuevo_usuario))
 
-    def alta_cliente_ind(self, clientes_individuos):
+    def alta_cliente_ind(self):
+        '''Método para dar de alta un cliente individuo, actualiza el el diccionario de clientes individuos
+        con el nuevo cliente y retorna un mensaje de éxito junto con el cliente creado'''
+
         dni = int(input("Número de documento del cliente: "))
 
-        for cliente in clientes_individuos:
-            if clientes_individuos[cliente].dni == int(dni):
+        # verificamos que el cliente no exista
+        for cliente in self.clientes_individuos:
+            if self.clientes_individuos[cliente].dni == int(dni):
                 return print("El cliente ya existe")
 
+        # solicitamos al usuario los datos del cliente
         apellido = input("Apellido del cliente: ")
         nombre = input("Nombre del cliente: ")
         cuit_cuil = input("CUIT/CUIL del cliente: ")
@@ -71,42 +83,57 @@ class Banco():
         # creamos la instancia de Cliente_individuo
         nuevo_cliente_ind = Cliente_individuo(
             apellido, nombre, dni, cuit_cuil, direccion, telefono, mail, id_cliente, cuentas)
-        clientes_individuos[id_cliente] = nuevo_cliente_ind
+
+        # actualiza el diccionario de clientes individuos
+        self.clientes_individuos[id_cliente] = nuevo_cliente_ind
 
         # creamos la instancia de Usuario
         self.alta_usuario(dni, id_cliente, usuarios)
 
         return print(f'\nEl cliente ha sido generado exitosamente: ' + nuevo_cliente_ind.__str__)
 
-    def alta_autoridad_firmante(self, id_cliente, usuarios):
+    def alta_autoridad_firmante(self, id_cliente):
+        '''Método para dar de alta una autoridad/firmante de una PyME, recibe el id de la PyME y el arreglo de usuarios
+        del sistema, lo actualiza con el usuario creado y retorna la instancia de la autoridad/firmante creada'''
+
         print(f'\nAlta de autoridad/firmante')
         dni = input("Número de documento: ")
 
-        for cliente in clientes_individuos:
-            if clientes_individuos[cliente].dni == int(dni):
+        # verificamos que la autoridad/firmante no exista
+        for autoridad in self.clientes_pyme:
+            if self.clientes_pyme[autoridad].dni == int(dni):
                 return print("Autoridad firmante existente")
 
+        # solicitamos al usuario los datos de la autoridad/firmante
         apellido = input("Apellido: ")
         nombre = input("Nombre: ")
         cuit_cuil = input("CUIT/CUIL: ")
         direccion = input("Dirección: ")
         telefono = input("Teléfono: ")
         mail = input("Email: ")
+
+        # creamos la instancia de Autoridad_firmante
         nueva_autoridad_firmante = Autoridad_firmante(
             apellido, nombre, dni, cuit_cuil, direccion, telefono, mail)
 
         # creamos la instancia de Usuario
-        self.alta_usuario(dni, id_cliente, usuarios)
+        self.alta_usuario(dni, id_cliente)
 
         return nueva_autoridad_firmante
 
-    def alta_cliente_pyme(self, clientes_pyme):
-        cuit_cuil = input("Número de CUIT/CUIL del cliente: ")
+    def alta_cliente_pyme(self):
+        '''Método para crear un cliente PyME, actualiza el diccionario de clientes PyME
+        con el nuevo cliente y retorna un mensaje de éxito junto con el cliente creado'''
 
-        for cliente in clientes_individuos:
-            if clientes_individuos[cliente].dni == cuit_cuil:
+        cuit_cuil = input(
+            "Número de CUIT/CUIL del cliente (sin guiones, sólo números): ")
+
+        # verificamos que el cliente no exista
+        for cliente in self.clientes_pyme:
+            if self.clientes_pyme[cliente].cuit_cuil == cuit_cuil:
                 return print("El cliente ya existe")
 
+        # solicitamos al usuario los datos del cliente
         razon_social = input("Razón social del cliente: ")
         direccion = input("Dirección del cliente: ")
         telefono = input("Teléfono del cliente: ")
@@ -116,6 +143,7 @@ class Banco():
         id_cliente = f'P{razon_social[0].upper()}{cuit_cuil[-4:-2]}{cuit_cuil[-1]}'
         cuentas = []  # lista de cuentas del cliente
 
+        # creamos la instancia de autoridad/es-firmante/s
         autoridades_firmantes = []
         agregar_aut_firmante = True
         while agregar_aut_firmante:
@@ -134,26 +162,96 @@ class Banco():
         # creamos la instancia de Cliente_pyme
         nuevo_cliente_pyme = Cliente_pyme(
             razon_social, cuit_cuil, direccion, telefono, mail, autoridades_firmantes, id_cliente, cuentas)
-        clientes_pyme[id_cliente] = nuevo_cliente_pyme
+
+        # actualizamos el diccionario de clientes PyME
+        self.clientes_pyme[id_cliente] = nuevo_cliente_pyme
+
         return print(f'\nEl cliente ha sido generado exitosamente: ' + Cliente_pyme.__str__(nuevo_cliente_pyme))
 
-    def modificar_cliente_ind(cliente_ind_a_modificar):
-        print('Modificación de datos del cliente: ')
-        cliente_ind_a_modificar.apellido = input("Apellido del cliente: ")
-        cliente_ind_a_modificar.nombre = input("Nombre del cliente: ")
-        cliente_ind_a_modificar.dni = int(
-            input("Número de documento del cliente: "))
-        cliente_ind_a_modificar.cuit_cuil = input("CUIT/CUIL del cliente: ")
-        cliente_ind_a_modificar.direccion = input("Dirección del cliente: ")
-        cliente_ind_a_modificar.telefono = input("Teléfono del cliente: ")
-        cliente_ind_a_modificar.mail = input("Email del cliente: ")
+    def menu_cuentas_usuario():
+        pass
 
-        return print(f'\nLos datos del cliente han sido modificados exitosamente: ' + cliente_ind_a_modificar.__str__)
+    def logueo_usuario(self):
+        '''Este método se encarga de loguear al usuario, recibe el usuario.
+        Retorna True si el usuario y la contraseña ingresados son correctos, False si no lo son.'''
 
-    def menu_administrador(self):
+        numero_usuario = input("Ingrese su numero de usuario")
+
+        # verificamos que el usuario ingresado exista
+        if numero_usuario in self.usuarios:
+            usuario = self.usuarios[numero_usuario]
+
+            clave = input("Ingrese su clave: ")
+            # veridficamos que la contraseña ingresada sea la correcta
+            if clave == usuario.clave:
+                self.usuario_logueado = self.clientes[usuario.id_cliente]
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def logueo_administrador(self):
+        '''Este método se encarga de loguear al administrador, recibe el usuario administrador.
+        Retorna True si el usuario y la contraseña ingresados son correctos, False si no lo son.'''
+
+        # verificamos que el usuario ingresado sea el administrador
+        nombre_usuario = input("Ingrese su nombre de usuario administrador")
+        if nombre_usuario != self.administrador.username:
+            return False
+
+        # verificamos que la contraseña ingresada sea la correcta
+        clave = input("Ingrese su clave: ")
+        if clave != self.administrador.clave:
+            return False
+
+        return True
+
+    def iniciar_sesion_administrador(self):
+        '''Este método se encarga de iniciar sesión al administrador, realiza el logueo del administrador
+        y si las credenciales son correctas, se invoca al método menu_administrador, si no, se informa
+        al usuario y se vuelve a llamar al método'''
+
+        if self.logueo_administrador():
+            self.menu_administrador()
+        else:
+            opcion = input(
+                "El logueo fue incorrecto, opcion 1 para seguir probando, 2 para salir")
+            if opcion == "1":
+                self.iniciar_sesion_administrador()
+            else:
+                exit()
+
+    def menu_usuario_cliente(self):
+        '''Este método se encarga de mostrar el menú de opciones del usuario, Si la opción es correcta,
+        se invoca al método en cuestión, si la opción ingresada no es correcta se imprime un mensaje de error.'''
+
         while True:
             opcion_seleccionada = int(input(
-                "Ingrese la opción: \n1: Monto de saldo retenido \n2: Monto de saldo descubierto \n3: Costos de servicios para cada tipo de transacción \n4: Porcentajes de beneficios para cada tipo de transacción \n5: Registrar cliente \n6: Registro de cuentas de cliente \n7: Cerrar sesión"))
+                "Ingrese la opción: \n1: Apertura de cuenta \n2: Cierre de cuenta \n3: Operar con cuentas\n4: Cerrar sesión"))
+
+            try:
+                if opcion_seleccionada == 1:
+                    self.cliente_logueado.apertura_cuenta()
+                elif opcion_seleccionada == 2:
+                    self.cliente_logueado.cierre_cuenta()
+                elif opcion_seleccionada == 3:
+                    self.menu_cuentas_usuario()
+                else:
+                    print("Opción incorrecta")
+            except ValueError:
+                print("La opción ingresada es inválida: escriba un numero entero")
+
+    def menu_administrador(self):
+        '''Método para mostrar el menú de opciones del administrador, recibe el usuario administrador.
+        Si la opción seleccionada es correcta, se invoca a la función en cuestión, si la opción ingresada no es correcta
+        se imprime un mensaje de error. Si elige la opción 2, se termina la ejecución del programa.'''
+
+        while True:
+            opcion_seleccionada = int(input(
+                "Ingrese la opción: \n1: Monto de saldo retenido \n2: Monto de saldo descubierto"
+                "\n3: Costos de servicios para cada tipo de transacción \n4: Porcentajes de beneficios para cada tipo de transacción"
+                "\n5: Registrar cliente \n6: Registro de cuentas de cliente \n7: Cerrar sesión"))
             try:
 
                 if opcion_seleccionada == 1:
@@ -175,87 +273,24 @@ class Banco():
             except ValueError:
                 print("La opción ingresada es inválida: escriba un numero entero")
 
-    def logueo_administrador(self):
-
-        nombre_usuario = input("Ingrese su nombre de usuario administrador")
-        if nombre_usuario != self.administrador.username:
-            return False
-
-        clave = input("Ingrese su clave: ")
-        if clave != self.administrador.clave:
-            return False
-
-        return True
-
-    def logueo_usuario(self):
-
-        numero_usuario = input("Ingrese su numero de usuario")
-
-        if numero_usuario in self.usuarios:
-            usuario = self.usuarios[numero_usuario]
-            clave = input("Ingrese su clave: ")
-
-            if clave == usuario.clave:
-                self.usuario_logueado = self.clientes[usuario.id_cliente]
-                return True
-            else:
-                return False
-        else:
-
-            return False
-
-    def iniciar_sesion_administrador(self):
-
-        if self.logueo_administrador():
-            self.menu_administrador()
-        else:
-            opcion = input(
-                "El logueo fue incorrecto, opcion 1 para seguir probando, 2 para salir")
-            if opcion == "1":
-                self.iniciar_sesion_administrador()
-            else:
-                exit()
-
-    def menu_cuentas_usuario():
-        pass
-
-    def menu_usuario_cliente_individuo(self):
-
-        opcion_seleccionada = int(input(
-            "Ingrese la opción: \n1: Apertura de cuenta \n2: Cierre de cuenta \n3: Operar con cuentas\n4: Cerrar sesión"))
-        if opcion_seleccionada == 1:
-            self.cliente_logueado.apertura_cuenta()
-
-        elif opcion_seleccionada == 2:
-            self.cliente_logueado.cierre_cuenta()
-
-        elif opcion_seleccionada == 3:
-            self.menu_cuentas_usuario()
-
     def menu(self):
         ''' La funcion menu solicita al usuario que ingrese una de las opciones indicadas.
-        Si la opción es correcta, se invoca a la función en cuestión, si la opción ingresada no es correcta se imprime un mensaje de error. Si elige la opción 2, se termina la ejecución del programa.'''
+        Si la opción es correcta, se invoca a la función en cuestión, si la opción ingresada no es correcta
+        se imprime un mensaje de error. Si elige la opción 2, se termina la ejecución del programa.'''
 
         while True:
-            opcionNro = input(
-                "Ingrese la opción: \n1: Iniciar sesión administrador \n2: Iniciar sesión usuario individuo \n3: Iniciar sesión usuario PyME \n4: Cerrar sesión")
+            opcion_seleccionada = input(
+                "Ingrese la opción: \n1: Iniciar sesión administrador  \n2: Iniciar sesión usuario "
+                "\n3: Cerrar sesión ")
 
             try:
-                if(int(opcionNro) == 1):
+                if(int(opcion_seleccionada) == 1):
                     self.iniciar_sesion_administrador()
-
-                elif(int(opcionNro) == 2):
+                elif(int(opcion_seleccionada) == 2):
                     if self.logueo_usuario():
                         print("Inicio de sesion correcto")
-                        self.menu_usuario_cliente_individuo()
-
-                elif(int(opcionNro) == 3):
-
-                    if self.logueo_usuario():
-                        print("Inicio de sesion correcto")
-                        self.menu_usuario_cliente_pyme()
-
-                elif(int(opcionNro) == 4):
+                        self.menu_usuario_cliente()
+                elif(int(opcion_seleccionada) == 3):
                     exit()
                 else:
                     print("Opción incorrecta")
