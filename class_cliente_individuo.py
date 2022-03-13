5264dc6aa627ffbe4240bc7a67da05cb02c121be
@pilarmoralescosta <maria.morales@globant.com>
2022-03-13 17:31:25 -0300

    crear caja de ahorro y cta cte, en la clase cliente

--- class_cliente_individuo.py
+++ class_cliente_individuo.py
@@ -12,51 +12,9 @@ class Cliente_individuo(Persona, Cliente):
                          direccion, telefono, email)
         Cliente.__init__(self, id_cliente, cuentas, registrado)
     
-    def abrir_cuenta_corriente(self):
-
-        abrir_cuenta = True
-
-        while abrir_cuenta:
-
-            sucursal = input("Ingrese el nombre de la sucursal: ")
-            nro_cuenta = random.randint(0, 10000000)
-            cbu = random.randint(0, 10000000)
-            fecha_apertura = datetime.date
-            saldo = 0
-            saldo_retenido = False
-            tipo = "Cuenta corriente"
-            cuenta = Cuenta_corriente(sucursal, nro_cuenta, cbu, fecha_apertura, saldo, tipo, saldo_retenido)
-            self.cuentas.append(cuenta)
-            print("Cuenta creada con exito")
-            print("Sus cuentas son", self.cuentas)
-            
-            return True
-
-    def abrir_caja_ahorro(self):
-
-        abrir_cuenta = True
-
-        while abrir_cuenta:
-
-            sucursal = input("Ingrese el nombre de la sucursal: ")
-            nro_cuenta = random.randint(0, 10000000)
-            cbu = random.randint(0, 10000000)
-            fecha_apertura = datetime.date
-            saldo = 0
-            saldo_retenido = False
-            tipo = "Caja de ahorro"
-            es_bonificada = True
-            cuenta = Caja_ahorro(sucursal, nro_cuenta, cbu, fecha_apertura, saldo, tipo, saldo_retenido, es_bonificada)
-            self.cuentas.append(cuenta)
-            print("Cuenta creada con exito")
-            print("Sus cuentas son", self.cuentas)
-            
-            return True
 
     
-    def abrir_caja_ahorro(self):
         
-        pass
 
     def mostrar_tipo_cliente(self, es_cliente_individuo, es_cliente_pyme):
         if es_cliente_individuo == True and es_cliente_pyme == False:
